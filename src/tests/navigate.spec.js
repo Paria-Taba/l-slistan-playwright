9cc8b9bf4a71abc80de0cd2d60bf71b5d05f11ff
paria <pariyataba@gmail.com>
2025-05-30 12:11:24 +0200

    test för katalog-vy

--- src/tests/navigate.spec.js
+++ src/tests/navigate.spec.js
@@ -5,10 +5,24 @@ test.describe("navigering", () => {
     await page.goto('https://tap-ht24-testverktyg.github.io/exam-template/');
   });
 
+  test("katalog-vy", async({page})=>{
+const buttonDisable=page.getByRole("button",{name:"katalog"})
+await expect (buttonDisable).toBeDisabled()
+
+	const text=page.getByText("Kim Vilsen")
+	await expect (text).toBeVisible()
+  })
+
 test("Klicka på 'Lägg till bok' och kontrollera att fältet 'Författare' visas", async ({ page }) => {
+
     await page.getByRole("button", { name: "Lägg till bok" }).click();
     const authorInput = page.getByTestId("add-input-author");
     await expect(authorInput).toBeVisible();
   });
+
+    test("mina-böcker-vy", async({page})=>{
+await page.getByRole("button",{name:"Mina böcker"}).click()
+
+  })
 
 });
